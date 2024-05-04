5fd11700bb597b59e82a45ee14cc3f48c80dcfab
yondikavl <yondikaviolanda@gmail.com>
2024-05-04 09:04:47 +0700

    fix: Layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import { EntityDetailContextProvider } from "@/components/utils/EntityDetailContext";
 import Head from "next/head";
-import Layout from "@/components/layout";
+import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   const { pathname } = useRouter();
