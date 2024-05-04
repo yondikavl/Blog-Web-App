247d123dfdb6c5e748a155538b57f26d85b3cf64
yondikavl <yondikaviolanda@gmail.com>
2024-05-04 09:20:40 +0700

    fix: Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,7 +6,7 @@ const rubik = Rubik({ subsets: ["latin"] });
 const Footer = () => {
   return (
     <main className={`${rubik.className}`}>
-      <div className="bg-blue-800 h-40 mt-20 text-center content-center">
+      <div className="bg-blue-800 h-40 mt-20 flex flex-col items-center justify-center">
         <h1 className="text-white text-2xl font-bold">Synap App</h1>
         <p className="text-white mt-4">Develop by Yondika Vio Landa</p>
       </div>
